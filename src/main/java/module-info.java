4035db13c4f7e5495c8cb1de812d8887bad488e3
Crasi10 <alexandru.crasovan2@student.upt.ro>
2025-03-25 14:58:36 +0200

    SM-1 Added the login screen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
-module com.example.studentmanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.studentmanager to javafx.fxml;
-    exports com.example.studentmanager;
+@Override
+public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+    primaryStage.setTitle("FIS - Student Manager");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
 }
